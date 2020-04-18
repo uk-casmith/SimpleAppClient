2e05418e2dc6e788a401ba63e4efe8178a989f80
Cameron Smith <gitcontrol@outlook.com>
2020-04-19 00:17:57 +0100

    add actions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+
 
 import App from './components/App';
 import Welcome from './components/Welcome';
+import Signup from './components/auth/Signup';
+import reducers from './reducers';
+
+const store = createStore(
+    reducers,
+    {},
+    applyMiddleware(reduxThunk)
+);
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App>
-            <Route path="/" exact component={ Welcome } />
-        </App>
-    </BrowserRouter>,
+    <Provider store={ store }>
+        <BrowserRouter>
+            <App>
+                <Route path="/" exact component={ Welcome } />
+                <Route path="/signup" exact component={ Signup } />
+            </App>
+        </BrowserRouter>
+    </Provider>,
     document.querySelector('#root')
 );
---
+++ src/reducers/auth.js
@@ -0,0 +1,7 @@
+const INITIAL_STATE = {
+    authenticated: '',
+    errorMessage: '',
+}
+export default (state = INITIAL_STATE, action) => {
+    return state;
+}; 
